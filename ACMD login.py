869d49b8cb6ebfa3778cff0e5e57ca4bd2cd5205
acmdcomp <116798885+acmdcomp@users.noreply.github.com>
2022-11-07 11:28:36 +0530

    Add files via upload

---
+++ ACMD login.py
@@ -0,0 +1,40 @@
+#Login
+login={}
+ct=0
+
+while True:
+    print("Hello! Welcome to the ACMD Database ")
+    print("Enter 1 if you are a new user")
+    print("Enter 2 if you are an existing user")
+    ch=int(input())
+    print()
+    print("Redirecting.....")
+
+    if ch==2:
+        username=input("Enter your username")
+        if username not in login:
+            print("Username not found")
+            break
+        while True:
+            if ct<5:
+                passwd=input("Enter your password")
+                if login[username]!=passwd:
+                    print("Password doesn't match")
+                    ct+=1
+                    break
+        login[username]=passwd
+        print("You have logged in successfully!")
+    if ch==1:
+        user=input("Enter your username")
+        while True:
+            for i in login:
+                if i==user:
+                    print("This user id already exists. Please try again")
+                    break
+        while True:
+            passw=input("Enter your password")
+            if len(passw)<8:
+                print("The password must have atleast 8 characters.")
+            else:
+                break
+            print("Your account has been created successfully.")
